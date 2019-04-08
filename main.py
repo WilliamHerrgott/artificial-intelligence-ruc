1b6ea67f868a0e145ecb1867b7a37b500ce58b1c
William Herrgott <herrgott.william@gmail.com>
2019-04-08 14:12:18 +0200

    Added some imports

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import csv
 
-fileName = ""
+from keras.models import Sequential, Model, InputLayer
+from keras.layers import Dense
+
+fileName = "bitcoin-historical-data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv"
 
 with open(fileName, 'r') as csvFile:
     data = csv.reader(csvFile)

8423354f89445095790f1640a82f2b46fc6e43fc
William Herrgott <herrgott.william@gmail.com>
2019-04-08 14:38:39 +0200

    Push develop

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import csv
+import pandas as pd
+import numpy as np
 
 from keras.models import Sequential, Model, InputLayer
 from keras.layers import Dense
 
+
+# change the name of this file to your own path
 fileName = "bitcoin-historical-data/coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv"
 
 with open(fileName, 'r') as csvFile:
